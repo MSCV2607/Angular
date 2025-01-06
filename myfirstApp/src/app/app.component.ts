e59bf6cf4775288f8b71dd46593338dacdd3115b
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-01-06 20:53:42 -0300

    Arreglos

--- myfirstApp/src/app/app.component.ts
+++ myfirstApp/src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-//Decorador
 @Component({
-  //En este caso tiene definido un selector, un template y un estilo (Archivo CSS)
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  // Asegúrate de usar 'styleUrls'
 })
-//Clase
 export class AppComponent {
   title = 'myfirstApp';
-
   saludo = 'Hola Mundo';
 }
