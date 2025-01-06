a90106ce81ec3c93fe96840d25b0e7ace5eef0dd
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-01-06 18:54:52 -0300

    Modificacion archivos Angular

--- myfirstApp/src/app/app.component.ts
+++ myfirstApp/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+//Decorador
 @Component({
+  //En este caso tiene definido un selector, un template y un estilo (Archivo CSS)
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+//Clase
 export class AppComponent {
   title = 'myfirstApp';
+
+  saludo = 'Hola Mundo';
 }
