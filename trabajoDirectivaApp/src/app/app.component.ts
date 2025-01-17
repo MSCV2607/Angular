d1844e303acccef2c366e712b71e53086431536c
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-01-17 19:25:25 -0300

    *ngFor

--- trabajoDirectivaApp/src/app/app.component.ts
+++ trabajoDirectivaApp/src/app/app.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Entrada {
+  nombre: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   titulo = 'Registro Usuarios';
-
   mensaje = '';
   registrado = false;
   nombre = '';
   apellido = '';
+  entradas: Entrada[] = [
+    { nombre: 'Juan' },
+    { nombre: 'Maria' },
+    { nombre: 'Pedro' }
+  ];
 
   registrarUsuario() {
-
     this.registrado = true;
-
     this.mensaje = 'Usuario registrado';
   }
 }
