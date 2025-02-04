ccb25b91489281ba94d1fc3aabb7dac50ec57e1b
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-02-04 18:56:14 -0300

    Explciacion de archivos

--- U/appAngular/src/app/app.component.ts
+++ U/appAngular/src/app/app.component.ts
@@ -1,10 +1,19 @@
+/*
+Este es el componente raíz de la aplicación. Dado que es un standalone component, 
+se define con standalone: true y se importa directamente en main.ts. Su selector 
+(<app-root>) se utiliza en index.html para renderizar la aplicación.
+*/
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  //Nombre del componente
   selector: 'app-root',
+  //Componente de tipo standalone
   standalone: true,
+  //Modulos que usaremos en nuestro componente
   imports: [RouterOutlet],
+  //Plantilla URL que vamos a usar
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- U/appAngular/src/main.ts
+++ U/appAngular/src/main.ts
@@ -1,3 +1,10 @@
+/*
+Este archivo sigue siendo el punto de entrada de la aplicación. 
+Sin embargo, en lugar de hacer bootstrap a un módulo (AppModule) 
+como se hace con aplicaciones basadas en Módulos, ahora se hace 
+directamente a un componente standalone.
+*/
+
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
