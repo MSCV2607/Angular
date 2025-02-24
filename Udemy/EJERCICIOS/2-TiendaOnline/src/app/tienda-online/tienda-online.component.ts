8375de17a4d81b4bab6d36b8279337d76fdcceaf
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-02-24 19:53:35 -0300

    Tienda online

---
+++ Udemy/EJERCICIOS/2-TiendaOnline/src/app/tienda-online/tienda-online.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tienda-online',
+  standalone: true,
+  imports: [CommonModule, FormsModule], 
+  templateUrl: './tienda-online.component.html',
+  styleUrl: './tienda-online.component.css'
+})
+export class TiendaOnlineComponent {
+  title = 'Lista de Productos';
+
+  productos: Producto[] = [
+    new Producto('Pantalón', 130.0),
+    new Producto('Camisa', 80.0),
+    new Producto('Playera', 50.0),
+  ];
+
+  descripcionInput: string = '';
+  precioInput: number = 0;
+
+  agregarProducto() {
+    if (this.descripcionInput.trim() === '' || this.precioInput <= 0) {
+      alert('Por favor ingrese una descripción y un precio válido.');
+      return;
+    }
+
+    this.productos.push(new Producto(this.descripcionInput, this.precioInput));
+
+    // Limpiar los campos después de agregar
+    this.descripcionInput = '';
+    this.precioInput = 0;
+  }
+}
+
+// Clase Producto
+export class Producto {
+  constructor(public descripcion: string, public precio: number) {}
+}
