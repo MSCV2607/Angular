68dfd513a77f940a0cf7e05941dcd0772fe6ce31
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-02-24 19:10:22 -0300

    Ejercicio Suma

---
+++ Udemy/EJERCICIOS/1-Calculadora/src/app/component/calculadora/calculadora.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-calculadora',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+
+  num1: number = 0;
+  num2: number = 0;
+  resultado: number = 0;
+
+  sumar() {
+    this.resultado = this.num1 + this.num2;
+  }
+
+}
