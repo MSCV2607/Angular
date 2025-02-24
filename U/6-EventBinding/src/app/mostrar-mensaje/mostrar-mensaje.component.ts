47999fe567fe7dc81489bbeab5bb13deb9c75fe3
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-02-24 15:58:04 -0300

    Probando EventBinding

---
+++ U/6-EventBinding/src/app/mostrar-mensaje/mostrar-mensaje.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-mensaje',
+  standalone: true,
+  imports: [],
+  templateUrl: './mostrar-mensaje.component.html',
+  styleUrl: './mostrar-mensaje.component.css'
+})
+export class MostrarMensajeComponent {
+mensaje: String = "";
+
+resetearMensaje() {
+ this.mensaje = "";
+}
+mostrarMensaje() {
+  this.mensaje = "Hola hiciste clic en el boton";
+}
+
+}
