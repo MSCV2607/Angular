e3c69d0f76540bb81c6c840dbd9f0177a8507281
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-03-05 16:25:25 -0300

    Ejercicio utilizacion input

--- Udemy/EJERCICIOS/1-Calculadora/src/app/component/calculadora/calculadora.component.ts
+++ Udemy/EJERCICIOS/1-Calculadora/src/app/component/calculadora/calculadora.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FormularioComponent } from "../formulario/formulario.component";
+import { ResultadoComponent } from "../resultado/resultado.component";
 
 @Component({
   selector: 'app-calculadora',
   standalone: true,
-  imports: [FormsModule, FormularioComponent],
+  imports: [FormsModule, FormularioComponent, ResultadoComponent],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.css'
 })
 export class CalculadoraComponent {
 
-  resultado: number | null = null;
+  resultadoPadre: number | null = null;
 procesarResultado(resultado: number) {
-this.resultado = resultado;
+this.resultadoPadre = resultado;
 }
  
 
